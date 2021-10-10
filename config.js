5914ae1589d231577d9845e908514b1f8db8a55f
Arjun5643 <71094214+Arjun5643@users.noreply.github.com>
2021-10-09 17:29:28 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAdJ2x1ainIbih35saDKbz2KhoKe8xl364",
+    authDomain: "team-voting-c2e8e.firebaseapp.com",
+    databaseURL: "https://team-voting-c2e8e-default-rtdb.firebaseio.com",
+    projectId: "team-voting-c2e8e",
+    storageBucket: "team-voting-c2e8e.appspot.com",
+    messagingSenderId: "1066498803538",
+    appId: "1:1066498803538:web:13acb89611deeb0c415c8b"
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
